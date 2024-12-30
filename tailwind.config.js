47f0bc731485fc0e93cae4e1984bd0a4e4e310a6
Betül Oran <116026974+betuloran@users.noreply.github.com>
2024-12-30 17:09:17 +0300

    tailwind updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ], theme: {
     extend: {
       colors: {
         primary: "#ffbe33",
         secondary: "#222831",
       },
       fontFamily: {
-        dancing: ["Dancing Script", "cursive"],
+        dancing:
+          ['Dancing Script', 'cursive'],
       },
     },
   },
