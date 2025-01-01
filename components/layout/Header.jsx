d2088ba02412c0499ff5bcce9faaf76c26856f0c
Betül Oran <116026974+betuloran@users.noreply.github.com>
2025-01-01 21:50:38 +0300

    project updated

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,16 +1,27 @@
 import Logo from "../ui/Logo";
-import { useState } from "react";
-
+import { useState } from "react"
+import { GiHamburgerMenu, GiCancel } from "react-icons/gi";
 import { FaUserAlt, FaShoppingCart, FaSearch } from "react-icons/fa";
 import Search from "../ui/Search";
+import { useRouter } from "next/router";
+
 const Header = () => {
     const [isSearchModal, setIsSearchModal] = useState(false);
+    const [isMenuModal, setIsMenuModal] = useState(false);
+
+    const router = useRouter();
+    console.log(router.asPath);
+
     return (
-        <div className="h-[5.5rem] bg-secondary">
+        <div className={`h-[5.5rem] z-50 relative ${router.asPath === "/" ? "bg-transparent" : "bg-secondary"
+            }`}>
             <div className="container mx-auto text-white flex justify-between items-center h-full">
                 <Logo />
-                <nav>
-                    <ul className="flex gap-x-2">
+                <nav
+                    className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-screen sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden  ${isMenuModal === true && "!grid place-content-center"
+                        }`}
+                >
+                    <ul className="flex gap-x-2 sm:flex-row flex-col items-center">
                         <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
                             <a href="">Home</a>
                         </li>
@@ -24,6 +35,14 @@ const Header = () => {
                             <a href="">Book Table</a>
                         </li>
                     </ul>
+                    {isMenuModal && (
+                        <button
+                            className="absolute  top-4 right-4 z-50"
+                            onClick={() => setIsMenuModal(false)}
+                        >
+                            <GiCancel size={25} className=" transition-all" />
+                        </button>
+                    )}
                 </nav>
                 <div className="flex gap-x-4 items-center">
                     <a href="#">
@@ -35,13 +54,19 @@ const Header = () => {
                     <button onClick={() => setIsSearchModal(true)}>
                         <FaSearch className="hover:text-primary transition-all" />
                     </button>
-                    <a href="#">
+                    <a href="#" className="md:inline-block hidden sm">
                         <button className="btn-primary">Order Online</button>
                     </a>
+                    <button
+                        className="sm:hidden inline-block"
+                        onClick={() => setIsMenuModal(true)}
+                    >
+                        <GiHamburgerMenu className="text-xl hover:text-primary transition-all" />
+                    </button>
                 </div>
             </div>
             {isSearchModal && (
-               <Search setIsSearchModal={setIsSearchModal} />
+                <Search setIsSearchModal={setIsSearchModal} />
             )}
         </div>
     );
