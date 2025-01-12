281bdc575ee7fa4860fd97a39ee36cfaac6472e9
Betül Oran <116026974+betuloran@users.noreply.github.com>
2025-01-12 22:06:38 +0300

    project ınput completed

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,11 +1,19 @@
 import Carousel from "@/components/ui/Carousel";
 import Campaigns from "../../components/Campaigns";
+import MenuWrapper from "../../components/product/MenuWrapper";
+import About from "../../components/About";
+import Reservation from "../../components/Reservation";
+
 
 const Index = () => {
     return (
         <div>
             <Carousel />
             <Campaigns />
+            <MenuWrapper />
+            <About />
+      <Reservation />
+
         </div>
     );
 };
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
-import Home from "./home";
+import Input from "../components/form/Input";
 import Header from "../components/layout/Header";
+import Home from "./home";
 
 export default function Index() {
   return (
@@ -19,6 +20,7 @@ export default function Index() {
       </Head>
       <Header />
       <Home />
+     
     </div>
   );
 }
