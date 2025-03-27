6e6110e14d87b03191a9ddf0d7760c77344fe19b
Betül Oran <116026974+betuloran@users.noreply.github.com>
2025-03-27 22:32:41 +0300

    about component updated

--- components/About.jsx
+++ components/About.jsx
@@ -1,3 +1,4 @@
+
 import Image from "next/image";
 import Title from "./ui/Title";
 const About = () => {
@@ -5,10 +6,11 @@ const About = () => {
         <div className="bg-secondary py-14">
             <div className="container mx-auto flex items-center text-white gap-20 justify-center flex-wrap-reverse">
                 <div className="flex justify-center">
-                    <div className="relative sm:w-[445px] sm:h-[600px]  flex justify-center w-[300px] h-[450px]">                        <Image src="/images/about-img.png" alt="" layout="fill" />
+                    <div className="relative sm:w-[445px] sm:h-[600px] flex justify-center w-[300px] h-[450px]">
+                        <Image src="/images/about-img.png" alt="" layout="fill" />
                     </div>
                 </div>
-                <div className="md:w-1/2 ">
+                <div className="md:w-1/2">
                     <Title addClass="text-[40px]">We Are Feane</Title>
                     <p className="my-5 flex flex-col items-center">
                         There are many variations of passages of Lorem Ipsum available, but
