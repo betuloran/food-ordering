ad619685b66b861845f61943684f76a8490c64e1
Betül Oran <116026974+betuloran@users.noreply.github.com>
2025-04-01 21:52:41 +0300

    Customer slider completed

--- components/customers/Customers.jsx
+++ components/customers/Customers.jsx
@@ -1,15 +1,57 @@
 import React from "react";
 import Title from "../ui/Title";
 import CustomerItem from "./CustomerItem";
+import Slider from "react-slick";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
 
 const Customers = () => {
+    function NextBtn({ onClick }) {
+        return (
+            <button className="text-white absolute flex items-center justify-center right-1/2 -bottom-12 bg-primary rounded-full p-2 mr-3" onClick={onClick}>
+                <IoIosArrowBack />
+            </button>
+        );
+    }
+    function PrevBtn({ onClick }) {
+        return (
+            <button className="text-white absolute flex items-center justify-center left-1/2 -bottom-12 bg-primary rounded-full p-2" onClick={onClick}>
+                <IoIosArrowForward />
+            </button>
+        );
+    }
+    const settings = {
+        dots: false,
+        arrow: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3500,
+        nextArrow: <NextBtn />,
+        prevArrow: <PrevBtn />,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
     return (
         <div className="container mx-auto my-20">
             <Title addClass="text-[40px] text-center">What Says Our Customers</Title>
-            <div className="flex gap-x-10">
+            <Slider {...settings}>
                 <CustomerItem imgSrc="/images/client1.jpg" />
                 <CustomerItem imgSrc="/images/client2.jpg" />
-            </div>
+                <CustomerItem imgSrc="/images/client1.jpg" />
+                <CustomerItem imgSrc="/images/client2.jpg" />
+                <CustomerItem imgSrc="/images/client1.jpg" />
+                <CustomerItem imgSrc="/images/client2.jpg" />
+            </Slider>
         </div>
     );
 };
