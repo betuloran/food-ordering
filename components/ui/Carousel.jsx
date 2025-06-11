c0de2b8bdd53124a9dffbab231a071e9803a5387
Betül Oran <116026974+betuloran@users.noreply.github.com>
2025-06-11 20:05:50 +0300

    product detail page completed

--- components/ui/Carousel.jsx
+++ components/ui/Carousel.jsx
@@ -27,7 +27,7 @@ const Carousel = () => {
       <div className="absolute top-0 left-0 w-full h-full">
         <div className="relative h-full w-full">
           <Image
-            src="/images/hero-bg.jpg"
+            src="/images/ff-5.jpg"
             alt=""
             layout="fill"
             objectFit="cover"
@@ -37,9 +37,9 @@ const Carousel = () => {
       </div>
       <Slider {...settings}>
         <div>
-          <div className="mt-48  text-white flex flex-col items-start gap-y-10">
-            <Title addClass="text-6xl">Fast Food Restaurant</Title>
-            <p className="text-sm sm:w-2/5 w-full">
+          <div className="mt-48 text-black flex flex-col items-start gap-y-10">
+            <Title addClass="text-6xl bg-white px-10 rounded-lg">Welcome!</Title>
+            <p className="text-sm sm:w-2/5 w-full bg-white rounded-lg p-3">
               Doloremque, itaque aperiam facilis rerum, commodi, temporibus
               sapiente ad mollitia laborum quam quisquam esse error unde.
               Tempora ex doloremque, labore, sunt repellat dolore, iste magni
@@ -49,9 +49,9 @@ const Carousel = () => {
           </div>
         </div>
         <div>
-          <div className="relative text-white top-48 flex flex-col items-start gap-y-10">
-            <Title addClass="text-6xl">Fast Food Restaurant</Title>
-            <p className="text-sm sm:w-2/5 w-full">
+          <div className="relative text-black top-48 flex flex-col items-start gap-y-10">
+            <Title addClass="text-6xl bg-white px-10 rounded-lg">Fast Food Restaurant</Title>
+            <p className="text-sm sm:w-2/5 w-full  bg-white rounded-lg p-3">
               Doloremque, itaque aperiam facilis rerum, commodi, temporibus
               sapiente ad mollitia laborum quam quisquam esse error unde.
               Tempora ex doloremque, labore, sunt repellat dolore, iste magni
